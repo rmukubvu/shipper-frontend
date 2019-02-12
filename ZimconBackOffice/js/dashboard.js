3705a88cbcb62bfdf5414f99262afa5ccf996dff
Pamela <mukubvur@outlook.com>
2019-02-12 17:06:30 +0200

    Remove duplicate menu Js Exception handling

--- ZimconBackOffice/js/dashboard.js
+++ ZimconBackOffice/js/dashboard.js
@@ -1,90 +1,91 @@
 /**
  * Created by robson on 2017/04/16.
  */
-$(document).ready(function() {     
-        
-        var consigneeId = localStorage.getItem('shipper.consignee');
-        if (consigneeId === ""){
-           //do nothing ... come with a plan for dashboard for back office     
-        }else{
-        
-            var getUrlVehicles = restEndPoint + "/vehicle";
-            var getUrlShipments = restEndPoint + "/shipment/consignee?consigneeId=" + consigneeId;
-            var getUrlShipmentsWithStatus = restEndPoint + "/shipment/status/consignee?consigneeId=" + consigneeId;
-            var getUrlConsigneeDetails = restEndPoint + "/consignee/id?id=" + consigneeId;
-            
-            var cachedShipmentsResults = localStorage.getItem('shipper.Shipments');          
-            if (cachedShipmentsResults === null || cachedShipmentsResults === ""){
-                $.ajax({
-                    type: "GET",
-                    dataType: "json",
-                    url: getUrlShipmentsWithStatus,
-                    success: function (result) {
-                        localStorage.setItem('shipper.Shipments',JSON.stringify(result)); 
-                        var trHTML = '';
-                        $.each(result, function (i, item) {                    
-                            var properIndex = i + 1;              
-                            trHTML += '<tr><td>' + properIndex + '.</td><td><a href=shipments.html?id=' + item.shipment.wayBillNumber + '>' + item.shipment.wayBillNumber + '</a></td><td>' + item.dashboardStatus.currentStatus + `</td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: ` + item.dashboardStatus.label + `"></div></div></td><td><span class="badge bg-green">` + item.dashboardStatus.label + '</span></td></tr>';
-                        });  
-                        $('#dashboard_table').append(trHTML);                          
-                    },
-                    error: function () {
-                        swal(
-                            'Ooops',
-                            'Something went terribly wrong! getUrlShipmentsWithStatus',
-                            'error'
-                        );
-                    }               
-                });
-            }else{
-                var result = JSON.parse(localStorage.getItem('shipper.Shipments'));
-                var trHTML = '';
-                $.each(result, function (i, item) {                    
-                    var properIndex = i + 1;              
-                    trHTML += '<tr><td>' + properIndex + '.</td><td><a href=shipments.html?id=' + item.shipment.wayBillNumber + '>' + item.shipment.wayBillNumber + '</a></td><td>' + item.dashboardStatus.currentStatus + `</td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: ` + item.dashboardStatus.label + `"></div></div></td><td><span class="badge bg-green">` + item.dashboardStatus.label + '</span></td></tr>';
-                });  
-                $('#dashboard_table').append(trHTML);                      
-            }
+$(document).ready(function () {
+    var restEndPoint = "http://154.0.174.76:8089";
 
+    var consigneeId = localStorage.getItem('shipper.consignee');
+    if (consigneeId === "") {
+        //do nothing ... come with a plan for dashboard for back office     
+    } else {
+
+        var getUrlVehicles = restEndPoint + "/vehicle";
+        var getUrlShipments = restEndPoint + "/shipment/consignee?consigneeId=" + consigneeId;
+        var getUrlShipmentsWithStatus = restEndPoint + "/shipment/status/consignee?consigneeId=" + consigneeId;
+        var getUrlConsigneeDetails = restEndPoint + "/consignee/id?id=" + consigneeId;
+
+        var cachedShipmentsResults = localStorage.getItem('shipper.Shipments');
+        if (cachedShipmentsResults === null || cachedShipmentsResults === "") {
             $.ajax({
                 type: "GET",
                 dataType: "json",
-                url: getUrlVehicles,
-                success: function (result) {                
-                    $.each(result, function (i, car) {
-                        localStorage.setItem(car.id, JSON.stringify(car));
-                    });                
-                },
-                error: function () {
-                    swal(
-                        'Ooops',
-                        'Something went wrong! getUrlVehicles',
-                        'error'
-                    );
-                }
-            });
-
-            //get consignee details
-            $.ajax({
-                type: "GET",
-                dataType: "json",
-                url: getUrlConsigneeDetails,
+                url: getUrlShipmentsWithStatus,
                 success: function (result) {
-                    $('#company_header_name').text(result.name);     
-                    var address = result.address + " " + result.address2 + " " + result.country;
-                    if ( address === null || address === ""){
-                        address = "Unit 11 City Deep Production Park 83 Heidelburg Road  City Deep 2049";                   
-                    }
-                    var gmapSource = "https://maps.google.com/maps?q=" + address + "&t=&z=15&ie=UTF8&iwloc=&output=embed";
-                    $('#gmap_canvas').attr("src",gmapSource);
+                    localStorage.setItem('shipper.Shipments', JSON.stringify(result));
+                    var trHTML = '';
+                    $.each(result, function (i, item) {
+                        var properIndex = i + 1;
+                        trHTML += '<tr><td>' + properIndex + '.</td><td><a href=shipments.html?id=' + item.shipment.wayBillNumber + '>' + item.shipment.wayBillNumber + '</a></td><td>' + item.dashboardStatus.currentStatus + `</td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: ` + item.dashboardStatus.label + `"></div></div></td><td><span class="badge bg-green">` + item.dashboardStatus.label + '</span></td></tr>';
+                    });
+                    $('#dashboard_table').append(trHTML);
                 },
                 error: function () {
                     swal(
                         'Ooops',
-                        'Something went wrong getUrlConsigneeDetails !' + result.message,
+                        'Something went terribly wrong! getUrlShipmentsWithStatus',
                         'error'
                     );
                 }
             });
-        }        
+        } else {
+            var result = JSON.parse(localStorage.getItem('shipper.Shipments'));
+            var trHTML = '';
+            $.each(result, function (i, item) {
+                var properIndex = i + 1;
+                trHTML += '<tr><td>' + properIndex + '.</td><td><a href=shipments.html?id=' + item.shipment.wayBillNumber + '>' + item.shipment.wayBillNumber + '</a></td><td>' + item.dashboardStatus.currentStatus + `</td><td><div class="progress progress-xs progress-striped active"><div class="progress-bar progress-bar-success" style="width: ` + item.dashboardStatus.label + `"></div></div></td><td><span class="badge bg-green">` + item.dashboardStatus.label + '</span></td></tr>';
+            });
+            $('#dashboard_table').append(trHTML);
+        }
+
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: getUrlVehicles,
+            success: function (result) {
+                $.each(result, function (i, car) {
+                    localStorage.setItem(car.id, JSON.stringify(car));
+                });
+            },
+            error: function () {
+                swal(
+                    'Ooops',
+                    'Something went wrong! getUrlVehicles',
+                    'error'
+                );
+            }
+        });
+
+        //get consignee details
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: getUrlConsigneeDetails,
+            success: function (result) {
+                $('#company_header_name').text(result.name);
+                var address = result.address + " " + result.address2 + " " + result.country;
+                if (address === null || address === "") {
+                    address = "Unit 11 City Deep Production Park 83 Heidelburg Road  City Deep 2049";
+                }
+                var gmapSource = "https://maps.google.com/maps?q=" + address + "&t=&z=15&ie=UTF8&iwloc=&output=embed";
+                $('#gmap_canvas').attr("src", gmapSource);
+            },
+            error: function () {
+                swal(
+                    'Ooops',
+                    'Something went wrong getUrlConsigneeDetails !' + result.message,
+                    'error'
+                );
+            }
+        });
+    }
 });
--- ZimconBackOffice/js/shipments.js
+++ ZimconBackOffice/js/shipments.js
@@ -1,28 +1,28 @@
 /**
  * Created by robson on 2017/04/17.
  */
-$(document).ready(function() {
+$(document).ready(function () {
 
-    if (localStorage.getItem('shipper.userName') === "" ){
+    if (localStorage.getItem('shipper.userName') === "") {
         location.href = "/Login";
         return;
     }
-         
+    var restEndPoint = "http://154.0.174.76:8089"; 
     var map;
     var map_marker;
-    var lat =-26.195246;
-    var lng =28.034088;
+    var lat = -26.195246;
+    var lng = 28.034088;
     var serialNumber;
     var lineCoordinatesArray = [];
     var vehicles = [];
-    
+
     //var url = new URL(window.location.href);
     //var waybillFromQueryString = url.searchParams.get("waybill");
     // user has directly clicked to this page without going through
     // the home page
-    var cachedShipmentsResults = localStorage.getItem('shipper.Shipments');          
-    if (cachedShipmentsResults === null || cachedShipmentsResults === ""){
-          //fetch from ajax call
+    var cachedShipmentsResults = localStorage.getItem('shipper.Shipments');
+    if (cachedShipmentsResults === null || cachedShipmentsResults === "") {
+        //fetch from ajax call
         var consigneeId = localStorage.getItem('shipper.consignee');
         var getUrlShipmentsWithStatus = restEndPoint + "/shipment/status/consignee?consigneeId=" + consigneeId;
         $.ajax({
@@ -30,41 +30,41 @@ $(document).ready(function() {
             dataType: "json",
             url: getUrlShipmentsWithStatus,
             success: function (result) {
-                localStorage.setItem('shipper.Shipments',JSON.stringify(result));
-                initialize();                                    
+                localStorage.setItem('shipper.Shipments', JSON.stringify(result));
+                initialize();
             },
             error: function () {
                 swal(
                     'Ooops',
-                    'Something went terribly wrong!',
+                    'Something went terribly wrong! /shipment/status/consignee?consigneeId= ',
                     'error'
                 );
-            }               
+            }
         });
     }
 
     var result = JSON.parse(localStorage.getItem('shipper.Shipments'));
     var trHTML = '';
-    $.each(result, function (i, item) {        
+    $.each(result, function (i, item) {
         trHTML += '<tr><td>' + displayVehicleDetails(item.shipment.vehicleId) + '</td><td>' + item.shipment.manifestReference + '</td><td>' + item.shipment.wayBillNumber + '</td><td><a href=javascript:void(0); onclick="displayStatusChanges(' + item.shipment.wayBillNumber + ');">View</a></td></tr>';
         if (vehicles.indexOf(item.shipment.vehicleId) === -1) {
             vehicles.push(item.shipment.vehicleId);
             initialize();
             displayMetrics(item.shipment.wayBillNumber);
-        }                
+        }
     });
-    $('#shipmentsDataTable').append(trHTML);        
+    $('#shipmentsDataTable').append(trHTML);
 
     /*if (waybillFromQueryString != null || waybillFromQueryString != 'undefined'){
         //query status using waybill
         displayStatusChanges(Number(waybillFromQueryString));
-    }*/    
-    
+    }*/
 
-    function displayVehicleDetails(vehicleId){
+
+    function displayVehicleDetails(vehicleId) {
         var json = JSON.parse(localStorage.getItem(vehicleId));
         return json.make + " - " + json.licenseId;
-    }    
+    }
 
     /*function getQueryVariable(variable) {
         var query = window.location.search.substring(1);
@@ -105,27 +105,31 @@ $(document).ready(function() {
         lineCoordinatesArray.push(new google.maps.LatLng(latIn, lngIn));
     }
 
-    function displayMetrics(waybill){
-        var urlAnalytics = restEndPoint + "/analytics?waybill=" + waybill;
+    function displayMetrics(waybill) {
+        var urlAnalytics = restEndPoint + "/analytics?waybill="+ waybill;
         $.ajax({
             type: "GET",
             dataType: "json",
             url: urlAnalytics,
-            success: function (result) {                
-               var message = "Distance from Origin : " + result.distanceFromOrigin + "\n" 
-               + "Distance to Destination : " + result.distanceToDestination + "\n" 
-               + "Total distance : " + result.completeDistanceToDestination + "\n" 
-               + "Estimated time of arrival : " + result.estimatedTimeOfArrival;
-               document.getElementById("analytics_details").value = message;
-               lat = parseFloat(result.latitude);
-               lng = parseFloat(result.longitude);
-               redraw();
+            success: function (result) {
+                if (result !== null) {
+                    var message = "Distance from Origin : " + result.distanceFromOrigin + "\n"
+                        + "Distance to Destination : " + result.distanceToDestination + "\n"
+                        + "Total distance : " + result.completeDistanceToDestination + "\n"
+                        + "Estimated time of arrival : " + result.estimatedTimeOfArrival;
+                    document.getElementById("analytics_details").value = message;
+                    lat = parseFloat(result.latitude);
+                    lng = parseFloat(result.longitude);
+                    redraw();
+                }else {
+                    document.getElementById("analytics_details").value = "";
+                }
             },
             error: function () {
                 swal(
-                    'Ooops',
-                    'Something went terribly wrong!',
-                    'error'
+                    'Result',
+                    'Truck at the depot',
+                    'warning'
                 );
             }
         });
